07c519afef849ac2f7ddc9e2b76c0c95ecd4ee9e
Kumari Anushka <anu.sb0340@gmail.com>
2022-02-21 22:56:28 +0530

    final

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,27 +4,35 @@ const { createTokens } = require("../jwt");
 
 const handleError = (err) => {
     console.log(err.message, err.code);
-    let errorMessage = "";
+    let errors = { success: false, email: '', password: '' };
 
-    // incorrect email/password
-    if (err.message === 'incorrect email' || err.message === 'incorrect password') {
-        errorMessage = 'Invalid Email or Password';
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
     }
 
     // duplicate email error
     if (err.code === 11000) {
-        errorMessage = 'User already exists';
-        return errorMessage;
+        errors.email = 'that email is already registered';
+        return errors;
     }
 
     // validation errors
     if (err.message.includes('user validation failed')) {
+        // console.log(err);
         Object.values(err.errors).forEach(({ properties }) => {
-            errorMessage = properties.message;
+            // console.log(val);
+            // console.log(properties);
+            errors[properties.path] = properties.message;
         });
     }
 
-    return errorMessage;
+    return errors;
 }
 
 module.exports.register_post = async (req, res) => {
@@ -57,7 +65,7 @@ module.exports.login_post = async (req, res) => {
             httpOnly: true,
         });
 
-        res.json({ auth: true, success: true, message: "Successfully Logged in" });
+        res.json({ auth: true, success: true, message: "Successfully Logged in",email });
     }
     catch (err) {
         let errors = handleError(err);
